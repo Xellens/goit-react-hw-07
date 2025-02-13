29749cc7f986fac516ec2ef64e11b1825cbbd242
Xelle <oksana2854x@gmail.com>
2025-02-13 12:44:44 +0100

    fix ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,16 +1,10 @@
 import { useSelector } from "react-redux";
 import Contact from "../Contact/Contact";
+import { selectFilteredContacts } from "../../redux/contactsSlice";
 import s from "./ContactList.module.css";
 
 function ContactList() {
-  const contacts = useSelector((state) => state.contacts.items);
-  const filter = useSelector((state) => state.filters.filter);
-
-  const normalizedFilter = filter ? filter.toLowerCase() : "";
-
-  const filteredContacts = contacts.filter((contact) =>
-    contact.name.toLowerCase().includes(normalizedFilter)
-  );
+  const filteredContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={s.list}>
